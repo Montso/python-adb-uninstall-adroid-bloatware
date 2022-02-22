7968305480e2e7f3202508f04bcfc47d6e9b5168
Montso Mokake <montshomokake@gmail.com>
2022-02-22 13:13:24 +0200

    Updated main.py with relevant comments

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ import subprocess
 import re
 import os
 
-os.chdir("C:/Users/monts/Downloads/platform-tools")
+adb_path = os.environ["ADB-PATH"]
+os.chdir(adb_path)  # go to folder with adb tool
 print(os.getcwd())
 
 def grep(cmd = ".\\adb shell pm list packages"):
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # This function grabs the output of command on the shell/terminal
+    # The default command is to list the (app) packages installed on the phone
+    
+    # variable out is the string from the output 
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # Argument cmd is your command
     out, err = p.communicate()
     out = out.decode("ascii")
+
+    # Turns each output line to a list item. Each line is delimited by \r\n
     out = out.split("\r\n")
 
     return out
 
-r = lambda x: re.search("facebook", x) != None
+uninstall_pkg = "facebook"
+r = lambda x: re.search(uninstall_pkg, x) != None  
 
 def main():
     out = grep()
-    pkg = list(filter(r, out)) 
+    pkg = list(filter(r, out))  # Filters to get only the packages belonging to Facebook app
     pkg = [x.removeprefix("package:") for x in pkg] # remove prefix "package:" 
     print(pkg)
     while len(pkg) != 0:
+        # The loop repeats until all unwanted packages have been uninstalled
+
         print(pkg[-1])
+
+        # Command to uninstall package: '.\adb shell pm uninstall --user 0 pkg_name'
         print(grep(".\\adb shell pm uninstall --user 0 {}".format(pkg[-1])))
         pkg.pop(-1)
 
